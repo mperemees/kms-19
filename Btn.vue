475302dd4920ca512c1a2aa90ce9921cd01f6cfa
Margaret Peremees <61801955+mperemees@users.noreply.github.com>
2020-11-26 23:28:55 +0200

    Add files via upload

---
+++ Btn.vue
@@ -0,0 +1,72 @@
+<template>
+<button class="button" :class="{
+  'is-primary': primary,
+  'is-info': info,
+  'is-white': white,
+  'is-light': light,
+  'is-dark': dark,
+  'is-black': black,
+  'is-text': text,
+  'is-ghost': ghost,
+  'is-link': link,
+  'is-success': success,
+  'is-warning': warning,
+  'is-danger': danger,
+  'is-small': small,
+  'is-normal': normal,
+  'is-medium': medium,
+  'is-large': large,
+  'is-fullwidth': fullwidth,
+  'is-outlined': outlined,
+  'is-inverted': inverted,
+  'is-rounded': rounded,
+  'is-hovered': hovered,
+  'is-focused': focused,
+  'is-active': active,
+  'is-loading': loading,
+  'is-static': staticButton,
+  'is-disabled': disabled,
+}"><slot></slot></button>
+</template>
+
+<script>
+export default {
+name: "Btn",
+  props: {
+    primary: Boolean,
+    info: Boolean,
+    white: Boolean,
+    light: Boolean,
+    dark: Boolean,
+    black: Boolean,
+    text: Boolean,
+    ghost: Boolean,
+    link: Boolean,
+    success: Boolean,
+    warning: Boolean,
+    danger: Boolean,
+    small: Boolean,
+    normal: Boolean,
+    medium: Boolean,
+    large: Boolean,
+    fullwidth: Boolean,
+    outlined: Boolean,
+    inverted: Boolean,
+    rounded: Boolean,
+    hovered: Boolean,
+    focused: Boolean,
+    active: Boolean,
+    loading: Boolean,
+    staticButton: Boolean,
+    disabled: Boolean,
+  }
+}
+</script>
+
+<style scoped>
+
+.button{
+  margin-top: 20px;
+}
+
+</style>
---
+++ ButtonExample.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="inner-container">
+    <btn info>Button</btn>
+    <btn primary>Button</btn>
+    <btn white>Button</btn>
+    <btn light>Button</btn>
+    <btn dark>Button</btn>
+    <btn black>Button</btn>
+    <btn text>Button</btn>
+    <btn ghost>Button</btn>
+    <br>
+    <btn link>Button</btn>
+    <btn success>Button</btn>
+    <btn warning>Button</btn>
+    <btn danger>Button</btn>
+    <btn primary light>Button</btn>
+    <btn link light>Button</btn>
+    <btn info light>Button</btn>
+    <btn success light>Button</btn>
+    <br>
+    <btn warning light>Button</btn>
+    <btn danger light>Button</btn>
+    <btn small>Button</btn>
+    <btn normal>Button</btn>
+    <btn medium>Button</btn>
+    <btn large>Button</btn>
+    <br>
+    <btn small fullwidth>Button</btn>
+    <br>
+    <btn normal fullwidth>Button</btn>
+    <br>
+    <btn medium fullwidth>Button</btn>
+    <br>
+    <btn large fullwidth>Button</btn>
+    <br>
+    <btn outlined>Button</btn>
+    <btn primary outlined>Button</btn>
+    <btn link outlined>Button</btn>
+    <btn info outlined>Button</btn>
+    <btn success outlined>Button</btn>
+    <btn danger outlined>Button</btn>
+    <btn primary inverted>Button</btn>
+    <btn link inverted>Button</btn>
+    <br>
+    <btn info inverted>Button</btn>
+    <btn success inverted>Button</btn>
+    <btn danger inverted>Button</btn>
+    <btn primary inverted outlined>Button</btn>
+    <btn link inverted outlined>Button</btn>
+    <btn info inverted outlined>Button</btn>
+    <btn success inverted outlined>Button</btn>
+    <btn danger inverted outlined>Button</btn>
+    <br>
+    <btn rounded>Button</btn>
+    <btn primary rounded>Button</btn>
+    <btn link rounded>Button</btn>
+    <btn info rounded>Button</btn>
+    <btn success rounded>Button</btn>
+    <btn danger rounded>Button</btn>
+    <br>
+    <btn hovered>Button</btn>
+    <btn primary hovered>Button</btn>
+    <btn link hovered>Button</btn>
+    <btn info hovered>Button</btn>
+    <btn success hovered>Button</btn>
+    <btn danger hovered>Button</btn>
+    <br>
+    <btn focused>Button</btn>
+    <btn primary focused>Button</btn>
+    <btn link focused>Button</btn>
+    <btn info focused>Button</btn>
+    <btn success focused>Button</btn>
+    <btn danger focused>Button</btn>
+    <br>
+    <btn active>Button</btn>
+    <btn primary active>Button</btn>
+    <btn link active>Button</btn>
+    <btn info active>Button</btn>
+    <btn success active>Button</btn>
+    <btn danger active>Button</btn>
+    <br>
+    <btn loading>Button</btn>
+    <btn primary loading>Button</btn>
+    <btn link loading>Button</btn>
+    <btn info loading>Button</btn>
+    <btn success loading>Button</btn>
+    <btn danger loading>Button</btn>
+    <btn static-button>Button</btn>
+  </div>
+
+
+</template>
+
+<script>
+import Btn from "./Btn";
+export default {
+name: "ButtonExample",
+  components: {Btn}
+}
+</script>
+
+<style scoped>
+.inner-container{
+  max-width: 1000px;
+}
+
+</style>
